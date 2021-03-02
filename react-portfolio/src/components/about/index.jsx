088f70a3d1732173e361ac80c2bb9081d94073b3
Ondayko14 <brandon@carmigo.io>
2021-03-01 21:03:50 -0600

    started an ABOUT ME header

--- react-portfolio/src/components/about/index.jsx
+++ react-portfolio/src/components/about/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function About() {
   return (
     <div class="about-me-container">
+      <h1>About Me</h1>
       <article class="about-me-wrapper" id="About">
           <p>I am a Front-End Developer currently enrolled in the Vanderbilt Coding Bootcamp.</p> 
           <p> I have experience in HTML/CSS and Javascript. Application development is a passion</p>
