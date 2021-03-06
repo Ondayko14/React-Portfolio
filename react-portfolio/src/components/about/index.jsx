da819aa225ce8b94486b2372f1d1ce40665b2eca
Ondayko14 <brandon@carmigo.io>
2021-03-06 13:35:26 -0600

    added hero to about

--- react-portfolio/src/components/about/index.jsx
+++ react-portfolio/src/components/about/index.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from "react";
+import Hero from "../hero";
 
 function About() {
   return (
-    <div className="about-me-container">
-      {console.log('insideAbout')}
-      <h1>About Me</h1>
-      <article className="about-me-wrapper" id="About">
-          <p>I am a Full Stack Developer currently enrolled in the Vanderbilt Coding Bootcamp.</p> 
-          <p> I have experience in Javascript/Node/NOSQL/SQL/React/Vue along with several CSS frameworks.</p> 
-          <p>Application development is a passion of mine, and it feels so good to get to do it on a daily basis.</p>
-          <p>I hope you enjoy browsing my projects and if you have any questions, please feel free to contact me.</p>
-      </article>
+    <div>
+      <Hero />
+      <div className="about-me-container">
+        {console.log("insideAbout")}
+        <h1>About Me</h1>
+        <article className="about-me-wrapper" id="About">
+          <p>
+            I am a Full Stack Developer currently enrolled in the Vanderbilt
+            Coding Bootcamp.
+          </p>
+          <p>
+            I have experience in Javascript/Node/NOSQL/SQL/React/Vue along with
+            several CSS frameworks.
+          </p>
+          <p>
+            Application development is a passion of mine, and it feels so good
+            to get to do it on a daily basis.
+          </p>
+          <p>
+            I hope you enjoy browsing my projects and if you have any questions,
+            please feel free to contact me.
+          </p>
+        </article>
+      </div>
     </div>
   );
 }
